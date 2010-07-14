d1dbcb1af5b77e2e63a2cfcb9ff9d200b1d84884
vinhboy <vinhboy@gmail.com>
2010-07-13 22:46:14 -0700

    with xpath

--- ImageskViewController.m
+++ ImageskViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ImageskViewController.h"
-
+#import "XPathQuery.h"
 
 @implementation ImageskViewController
 
@@ -32,34 +32,81 @@
 	UIImage* image = [info valueForKey:@"UIImagePickerControllerOriginalImage"];
     
 	self.imageData = UIImageJPEGRepresentation(image, 0.85f);
-	
-	responseData = [NSMutableData data];
-	
-	NSLog(@"%@", @"HI");
-	//[s release];	
-	
-	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:8080/?upload_url=true"]];
-	// NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:8080/?upload_url=true"]];
-	[NSURLConnection connectionWithRequest:request delegate:self];
-	
-	// NSString *song = [[NSString alloc] initWithData:responseData encoding:NSASCIIStringEncoding];
-	NSLog(@"%@", responseData);
-	
-	NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"nothing"]];
-	[urlRequest setHTTPMethod:@"POST"];
-    [urlRequest setValue: [NSString stringWithFormat:@"multipart/form-data; boundary=%@", @"0xKhTmLbOuNdArY"] forHTTPHeaderField:@"Content-Type"];
-	
-    NSMutableData *postData = [NSMutableData dataWithCapacity:[self.imageData length] + 512];
-    [postData appendData: [[NSString stringWithFormat:@"--%@\r\n", @"0xKhTmLbOuNdArY"] dataUsingEncoding:NSUTF8StringEncoding]];
-    [postData appendData: [[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"file.jpg\"; Content-Type: image/jpeg\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-    [postData appendData: self.imageData];
-    [postData appendData: [[NSString stringWithFormat:@"\r\n--%@--\r\n", @"0xKhTmLbOuNdArY"] dataUsingEncoding:NSUTF8StringEncoding]];
-	
-    [urlRequest setHTTPBody:postData];
-	
-	[NSURLConnection connectionWithRequest:urlRequest delegate:self];
-}
 
+	// Create the request.
+	NSURLRequest *theRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:8080/"] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:160.0];
+	
+	// create the connection with the request
+	// and start loading the data
+	NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+	
+	if (theConnection) {
+		NSLog(@"Retrieving upload url...");
+		// Create the NSMutableData to hold the received data.
+		// receivedData is an instance variable declared elsewhere.
+		responseData = [[NSMutableData data] retain];
+	} else {
+		// Inform the user that the connection failed.
+	}
+}
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [responseData setLength:0];
+}
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    // Append the new data to receivedData.
+    // receivedData is an instance variable declared elsewhere.
+    [responseData appendData:data];
+}
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+	NSString *responseString = [[[NSString alloc] initWithBytes:[self.responseData bytes]
+														 length:[self.responseData length]
+													   encoding:NSUTF8StringEncoding] autorelease];
+	NSLog(@"Upload URL: %@",responseString);
+	
+	NSString *xpathQueryString = @"//div[@id='bd']/form";
+	NSArray *results = PerformHTMLXPathQuery(self.responseData, xpathQueryString);
+	
+	NSLog(@"Libxml Key: %@",[[results objectAtIndex:0] objectForKey:@"nodeAttributeArray"]);
+	
+	NSArray *result = [[results objectAtIndex:0] objectForKey:@"nodeAttributeArray"];
+	
+	for(NSDictionary * attr in result) {
+		NSLog(@"AttributeName: %@",[attr objectForKey:@"attributeName"]);
+		if([[attr objectForKey:@"attributeName"] isEqualToString:@"action"]) {
+			NSLog(@"Attr: %@",[attr objectForKey:@"nodeContent"]);
+		}
+	}
+	
+//	if (responseString) {
+//		NSString *boundary = @"---------------------------iMagesk-mUlTiPaRtFoRm";
+//		
+//		NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:responseString]];
+//		[urlRequest setHTTPMethod:@"POST"];
+//		[urlRequest setValue: [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary] forHTTPHeaderField:@"Content-Type"];
+//		 
+//		NSMutableData *postData = [NSMutableData data];
+//		[postData appendData: [[NSString stringWithFormat:@"--%@\r\nContent-Disposition: form-data; name=\"file\"; filename=\"imageskApp.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+//		[postData appendData: self.imageData];
+//		[postData appendData: [[NSString stringWithFormat:@"\r\n--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+//		 
+//		[urlRequest setHTTPBody:postData];
+//		[NSURLConnection connectionWithRequest:urlRequest delegate:self];
+//	}
+}
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    // release the connection, and the data object
+    [connection release];
+    [responseData release];
+	
+    // inform the user
+    NSLog(@"Connection failed! Error - %@ %@",
+          [error localizedDescription],
+          [[error userInfo] objectForKey:NSErrorFailingURLStringKey]);
+}
 /*
  // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
